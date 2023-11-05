6e62c2daddd1c3252b9f1c77c3391c27f3405e15
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2023-11-05 20:43:32 +0900

    https://github.com/toposoid/toposoid-deduction-protocol-model/issues/59

--- src/main/scala/com/ideal/linked/toposoid/protocol/model/base/CoveredPropositionNode.scala
+++ src/main/scala/com/ideal/linked/toposoid/protocol/model/base/CoveredPropositionNode.scala
@@ -16,19 +16,16 @@
 
 package com.ideal.linked.toposoid.protocol.model.base
 
-import com.ideal.linked.toposoid.knowledgebase.model.{KnowledgeBaseEdge, KnowledgeBaseNode, KnowledgeBaseSemiGlobalNode}
 import play.api.libs.json.{Json, OWrites, Reads}
 
 /**
  *
- * @param nodeMap
- * @param edgeList
- * @param knowledgeBaseSemiGlobalNode
- * @param deductionResult
+ * @param currentId
+ * @param propositionCurrentId
+ * @param surface
  */
-case class AnalyzedSentenceObject(nodeMap:Map[String, KnowledgeBaseNode], edgeList:List[KnowledgeBaseEdge], knowledgeBaseSemiGlobalNode: KnowledgeBaseSemiGlobalNode, deductionResult:DeductionResult)
-object AnalyzedSentenceObject {
-  implicit val jsonWrites: OWrites[AnalyzedSentenceObject] = Json.writes[AnalyzedSentenceObject]
-  implicit val jsonReads: Reads[AnalyzedSentenceObject] = Json.reads[AnalyzedSentenceObject]
+case class CoveredPropositionNode(currentId:String, propositionCurrentId:String, surface:String)
+object CoveredPropositionNode {
+  implicit val jsonWrites: OWrites[CoveredPropositionNode] = Json.writes[CoveredPropositionNode]
+  implicit val jsonReads: Reads[CoveredPropositionNode] = Json.reads[CoveredPropositionNode]
 }
-
