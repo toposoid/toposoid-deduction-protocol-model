32fc2153eba94562f5d784528d3070c932b0727f
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2024-08-19 18:45:52 +0900

    https://github.com/toposoid/toposoid-deduction-protocol-model/issues/80

--- src/main/scala/com/ideal/linked/toposoid/protocol/model/redis/KeyValueStoreInfo.scala
+++ src/main/scala/com/ideal/linked/toposoid/protocol/model/redis/KeyValueStoreInfo.scala
@@ -19,7 +19,7 @@ package com.ideal.linked.toposoid.protocol.model.redis
 import play.api.libs.json.{Json, OWrites, Reads}
 
 case class KeyValueStoreInfo(identifier:String, key:String, value:String)
-object UserInfo{
+object KeyValueStoreInfo{
   implicit val jsonWrites: OWrites[KeyValueStoreInfo] = Json.writes[KeyValueStoreInfo]
   implicit val jsonReads: Reads[KeyValueStoreInfo] = Json.reads[KeyValueStoreInfo]
 }
