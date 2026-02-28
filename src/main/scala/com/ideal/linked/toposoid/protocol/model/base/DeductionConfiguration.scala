5343c49e59cfe4fe0d10153b4a88e7e7dae56b20
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-02-28 21:33:51 +0000

    https://github.com/toposoid/toposoid-deduction-protocol-model/issues/109

--- src/main/scala/com/ideal/linked/toposoid/protocol/model/base/DeductionConfiguration.scala
+++ src/main/scala/com/ideal/linked/toposoid/protocol/model/base/DeductionConfiguration.scala
@@ -2,7 +2,7 @@ package com.ideal.linked.toposoid.protocol.model.base
 
 import play.api.libs.json.{Json, OWrites, Reads}
 
-final case class DeductionConfiguration(actionModeType:Int, llmModel:String, llmModelHyperParameters:Map[String, String])
+final case class DeductionConfiguration(actionModeType:Int, llmModel:String, llmModelHyperParameters:Map[String, String], maxTargetKnowledgeCount:Int=10)
 object DeductionConfiguration {
   implicit val jsonWrites: OWrites[DeductionConfiguration] = Json.writes[DeductionConfiguration]
   implicit val jsonReads: Reads[DeductionConfiguration] = Json.reads[DeductionConfiguration]
