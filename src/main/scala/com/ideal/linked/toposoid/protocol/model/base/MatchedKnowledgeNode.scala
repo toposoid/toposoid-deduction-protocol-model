8589f170a9334748115e9f09d591699b1522708e
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-03-04 01:24:41 +0000

    https://github.com/toposoid/toposoid-deduction-protocol-model/issues/109

--- src/main/scala/com/ideal/linked/toposoid/protocol/model/base/MatchedKnowledgeNode.scala
+++ src/main/scala/com/ideal/linked/toposoid/protocol/model/base/MatchedKnowledgeNode.scala
@@ -2,7 +2,7 @@ package com.ideal.linked.toposoid.protocol.model.base
 
 import play.api.libs.json.{Json, OWrites, Reads}
 
-case class MatchedKnowledgeNode(nodeId:String, caseNameOnEdge:String, isDenialWord:Boolean, nodeType: Int)
+case class MatchedKnowledgeNode(sentenceId:String, nodeId:String, caseNameOnEdge:String, isDenialWord:Boolean, nodeType: Int)
 
 object MatchedKnowledgeNode {
   implicit val jsonWrites: OWrites[MatchedKnowledgeNode] = Json.writes[MatchedKnowledgeNode]
