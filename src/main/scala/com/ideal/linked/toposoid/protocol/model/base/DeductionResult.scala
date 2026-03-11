142053a7d74f402f26e17f3a74200849d0e1bbaf
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-03-11 02:28:22 +0000

    https://github.com/toposoid/toposoid-deduction-protocol-model/issues/109

--- src/main/scala/com/ideal/linked/toposoid/protocol/model/base/DeductionResult.scala
+++ src/main/scala/com/ideal/linked/toposoid/protocol/model/base/DeductionResult.scala
@@ -20,14 +20,14 @@ package com.ideal.linked.toposoid.protocol.model.base
 import play.api.libs.json.{Json, OWrites, Reads}
 
 /**
- *
- * @param status
- * @param coveredPropositionResults
- * @param havePremiseInGivenProposition parameter is valid only when SentenceType is Claim.
- *　　　　　　　　　　　　　　　　　　　　　　　This parameter will be True only if the proposition has a Premise and
- *                                      there is a corresponding Claim in Knowledgebase that is related to the Proposition's Premise.
- */
-case class DeductionResult(status:Boolean, authenticityType:Int, coveredPropositionResults:List[CoveredPropositionResult], havePremiseInGivenProposition:Boolean = false, deductionPhaseType:Int = 1)
+  * 
+  * @param status
+  * @param coveredPropositionResults
+  * @param havePremiseInGivenProposition parameter is valid only when SentenceType is Claim. This parameter will be True only if the proposition has a Premise and there is a corresponding Claim in Knowledgebase that is related to the Proposition's Premise.
+  * @param deductionPhaseType ref. com.ideal.linked.toposoid.common.DeductionPhaseType
+  * @param authenticityType ref. com.ideal.linked.toposoid.common.AuthenticityType
+  */
+case class DeductionResult(status:Boolean, coveredPropositionResults:List[CoveredPropositionResult], havePremiseInGivenProposition:Boolean = false, deductionPhaseType:Int = 1, authenticityType:Int = 2)
 object DeductionResult {
   implicit val jsonWrites: OWrites[DeductionResult] = Json.writes[DeductionResult]
   implicit val jsonReads: Reads[DeductionResult] = Json.reads[DeductionResult]
