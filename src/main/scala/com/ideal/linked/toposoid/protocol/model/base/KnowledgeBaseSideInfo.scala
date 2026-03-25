5d9e2b9cf16a2e7837be420999d9a02775b101b1
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-03-25 03:40:05 +0000

    https://github.com/toposoid/toposoid-deduction-protocol-model/issues/109

--- src/main/scala/com/ideal/linked/toposoid/protocol/model/base/KnowledgeBaseSideInfo.scala
+++ src/main/scala/com/ideal/linked/toposoid/protocol/model/base/KnowledgeBaseSideInfo.scala
@@ -19,7 +19,7 @@ package com.ideal.linked.toposoid.protocol.model.base
 
 import play.api.libs.json.{Json, OWrites, Reads}
 
-case class KnowledgeBaseSideInfo(propositionId:String, sentenceId:String, featureInfoList:List[MatchedFeatureInfo])
+case class KnowledgeBaseSideInfo(propositionId:String, sentenceId:String, featureInfoList:List[MatchedFeatureInfo], deductionUnits:List[String])
 
 object KnowledgeBaseSideInfo {
   implicit val jsonWrites: OWrites[KnowledgeBaseSideInfo] = Json.writes[KnowledgeBaseSideInfo]
